6ace0bfed60345524f7f6df57e7984c54ae3b2a4
MattSokol79 <maciej.sokol79@Gmail.com>
2020-11-02 16:56:31 +0000

    String and Concatenation Updated

--- strings_concatenation_&_casting.py
+++ strings_concatenation_&_casting.py
@@ -27,18 +27,71 @@ print(int(number) + age)
 name = input("Please enter your name ")
 print("Hello " + name)
 
-# Get user data
+# EXERCISE: Get user data
 # Display message (Your age is + )
 # Address - including post code and first line of address - house number
 # DOB
 
 user_name = str(input("Please enter your full name "))
 user_age = int(input("Please enter your age "))
-user_address = str(input("Please enter your first line of address "))
-user_postcode = str(input("Please enter your postcode "))
 user_housenumber = int(input("Please enter your house number "))
+user_address = str(input("Please enter your street name"))
+user_postcode = str(input("Please enter your postcode "))
 user_dob = str(input("Please enter your date of birth in the format dd/mm/yy "))
 
 print("Hello " + user_name, "your age is " + str(user_age), "your full address is "
       + str(user_housenumber), user_address, user_postcode, "and your date of birth is "
       + user_dob)
+
+# Or do f" formatting like the example below
+print(f"Hello {user_name} yor age is {str(user_age)}, your full address is {str(user_housenumber)}"
+      f"{user_address} {user_postcode} and your date of birth is {user_dob}")
+
+# Declaring strings with double and single quotes
+# "" as well as ''
+single_quotes = 'single_quotes\'wow\''
+print(single_quotes)
+
+double_quotes = "double quotes 'wow' "
+print(double_quotes)
+
+
+
+
+# String slicing - Why?
+# Indexing in python starts from 0
+greetings = "Hello World!"
+
+# We only want to print out Hello, prints out from H - space so Hello
+print(greetings[0:5])
+
+# How to find the length of string? len()
+print(len(greetings))
+
+# How to get rid of these spaces at the end? Its necessary as it consumes memory
+white_spaces = "lot's of spaces at the end                                     "
+print(len(white_spaces))
+print(white_spaces.strip())
+
+# strip() deletes all the spaces at the end of a string
+print(len(white_spaces.strip()))
+
+
+
+
+# count() counts number of times any word is available in the string
+example_text = "lots of text with Some text "
+print(example_text.count("text")
+# The output is 2 as the word "text" is in the string twice
+
+# To replace something
+print(example_text.replace("with", ",")) # Replace the "with" and put ","
+
+# Capitalise the first letter of the sentence
+print(example_text.capitalize())
+
+# Change everything to upper case
+print(example_text.upper())
+
+# Change everything to lower case
+print(example_text.lower())
