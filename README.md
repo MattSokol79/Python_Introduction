0736442f97473359b5084dd9c547f225717ee97f
MattSokol79 <maciej.sokol79@Gmail.com>
2020-11-02 15:05:14 +0000

    Python Variable

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Python 
+## Pycharm setup
+### python 3.7
+### Python Variable
+
+**Objectives**
+- What is a variable
+- Dynamically typed languages
+- Overwriting variables
+- Getting user input from the command line
+
+### Variable
+It is a place holder to store data
+
+### Pseudocoding
+Essentially, it is a translation of your code
--- python_variables.py
+++ python_variables.py
@@ -1,5 +1,31 @@
-import numpy as np
-a = np.array[12,23]
+# How to create a variable
+# If you want to comment out more than one line, highlight it all and CTRL + /
 
-print(a)
-print('Hello World')
+name = "Matt" # String
+# Creating a variable called name to store user name
+
+age = 22 # Int
+# Creating a variable called age to store age of user
+
+hourly_wage = 10 # Int
+# Creating a variable called hourly_wage to store the hourly wage of the user
+
+travel_allowance = 2.1 # Float
+# Creating a variable called travel_allowance to store the travel allowance of the user
+
+# Python has string, int, float and boolean values
+# How to find out the type of variable?
+# => type() gives us the actual type of value
+print(travel_allowance)
+print(age)
+print(hourly_wage)
+
+
+# How to take user data
+# Use input() to get data from users
+
+name1 = str(input("Please enter your name "))
+# Getting user data by using input() method
+print(name1)
+
+
